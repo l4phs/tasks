39366194b4f9ee81b209538035e6de9e5b498857
l4phs <laphs@udel.edu>
2024-02-13 18:45:22 -0500

    Finished task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Container, Row, Col } from "react-bootstrap";
+import Megamoid2 from "./Megamoid2.png";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 export function App(): JSX.Element {
     return (
@@ -23,11 +24,40 @@ export function App(): JSX.Element {
                 <Row>
                     <Col>
                         <img
-                            src="src/Megamoid2.jpg"
+                            src={Megamoid2}
                             alt="This is a picture of Megamind from the hit dreamworks movie Megamind"
                         />{" "}
                     </Col>
-                    <Col> Second Col </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            {" "}
+                            Log Hello World
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div style={{ backgroundColor: "red" }}>
+                            <span style={{ color: "red" }}> Rectangle 1 </span>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div style={{ backgroundColor: "red" }}>
+                            <span style={{ color: "red" }}> Rectangle 2 </span>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ol>
+                            <li>One</li>
+                            <li>Two</li>
+                            <li>Three</li>
+                            <li>Four</li>
+                        </ol>
+                    </Col>
                 </Row>
             </Container>
         </div>
