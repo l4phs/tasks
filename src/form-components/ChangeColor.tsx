db472fa6a53c8a8ce9583dc38f16c0b275dc8e88
l4phs <laphs@udel.edu>
2024-03-12 13:19:21 -0400

    re-adding task 10, hopefully no linting errors

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,54 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
+//need to update
 export function ChangeColor(): JSX.Element {
+    const [selectedColor, setSelectedColor] = useState<string>("red"); // Set the initial color
+
+    const colors = [
+        "red",
+        "blue",
+        "green",
+        "orange",
+        "purple",
+        "yellow",
+        "magenta",
+        "black"
+    ];
+
+    const handleColorChange = (color: string) => {
+        setSelectedColor(color === selectedColor ? "on" : color); // Always set to 'on' on button click
+    };
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((color) => (
+                <div key={color} className="form-check-inline">
+                    <Form.Check
+                        type="radio"
+                        id={`color-${color}`}
+                        name="color"
+                        value={color}
+                        checked={selectedColor === color}
+                        onChange={() => handleColorChange(color)}
+                        className="form-check-input"
+                    />
+                    <label
+                        htmlFor={`color-${color}`}
+                        style={{ backgroundColor: color }}
+                    >
+                        {color}
+                    </label>
+                </div>
+            ))}
+            <div>
+                You have chosen{" "}
+                <span
+                    data-testid="colored-box"
+                    style={{ backgroundColor: selectedColor }}
+                >
+                    {selectedColor}
+                </span>
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isAStudent, setStudent] = useState<boolean>(true);
+
+    function updateEdit(): void {
+        setEditMode(!editMode);
+    }
+    function updateStudent(): void {
+        setStudent(!isAStudent);
+    }
+    function updateUserName(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserName(event.target.value);
+    }
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="edit-mode"
+                checked={editMode}
+                onChange={updateEdit}
+            />
+            {editMode ? (
+                <div>
+                    <Form.Control
+                        type="text"
+                        value={userName}
+                        onChange={updateUserName}
+                    />
+                    <Form.Check
+                        type="checkbox"
+                        id="is-student"
+                        label="Are you a Student?"
+                        checked={isAStudent}
+                        onChange={updateStudent}
+                    />
+                </div>
+            ) : (
+                <p>
+                    {userName} is
+                    {isAStudent ? " a student" : " not a student"}
+                </p>
+            )}
         </div>
     );
 }
